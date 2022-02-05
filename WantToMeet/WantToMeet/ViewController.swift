72a85ca6066c212028f190725ae4217485fd470a
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-02-05 17:46:43 -0300

    Creating application WantToMeet
    Iniciando mais um aplicativo, feito a criação do app e a inserção das imagens que serão utilizadas e a criação da launchScreen

---
+++ WantToMeet/WantToMeet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WantToMeet
+//
+//  Created by Rennan Bruno on 05/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
