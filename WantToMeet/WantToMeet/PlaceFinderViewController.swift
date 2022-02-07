20c343d013df04e882b63e28d4db406c27f854c1
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-02-06 23:33:02 -0300

    Map Configuration
    Adicionando o mapa e sua pesquisa no aplicativo.

--- WantToMeet/WantToMeet/PlaceFinderViewController.swift
+++ WantToMeet/WantToMeet/PlaceFinderViewController.swift
@@ -9,13 +9,18 @@ import UIKit
 import MapKit
 
 class PlaceFinderViewController: UIViewController {
+    
+    enum PlaceFinderMessageType {
+        case error(String)
+        case confirmation(String)
+    }
 
     @IBOutlet weak var tfCity: UITextField!
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var aiLoading: UIActivityIndicatorView!
     @IBOutlet weak var viLoading: UIView!
     
-    
+    var place: Place!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,9 +36,60 @@ class PlaceFinderViewController: UIViewController {
         let geoCoder = CLGeocoder()
         geoCoder.geocodeAddressString(address) { (placemarks, error) in
             self.load(show: false)
-            guard let placemark = placemarks?.first else {return}
-            print(Place.getFormattedAddress(with: placemark))
+            if error == nil {
+                if !self.savePlace(with: placemarks?.first){
+                    self.showMessage(type: .error("Não foi encontrado nenhum local com esse nome"))
+                }
+            } else {
+                self.showMessage(type: .error("Erro desconhecido"))
+            }
+            
+            
         }
+    }
+    
+    func savePlace(with placemark: CLPlacemark?) -> Bool {
+        guard let placemark = placemark, let coordinate = placemark.location?.coordinate else {
+            return false
+        }
+        let name = placemark.name ?? placemark.country ?? "Desconhecido"
+        let address = Place.getFormattedAddress(with: placemark)
+        place = Place(name: name, latitude: coordinate.latitude, longitude: coordinate.latitude, address: address)
+        
+        let region = MKCoordinateRegion(center: coordinate, latitudinalMeters: 3500, longitudinalMeters: 3500)
+        mapView.setRegion(region, animated: true)
+        
+        self.showMessage(type: .confirmation(place.name))
+        
+        return true
+    }
+    
+    func showMessage(type: PlaceFinderMessageType) {
+        let title: String, message: String
+        var hasConfirmation: Bool = false
+        
+        switch type {
+            case .confirmation(let name):
+                title = "Local Encontrado"
+                message = "Deseja adicionar \(name) ?"
+                hasConfirmation = true
+            case .error(let errorMessage):
+                title = "Erro"
+                message = errorMessage
+        }
+        //Criando o Alert e configurando os botões
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        //Configurando e adicionando o botão de cancelamento
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        //Configurando e adicionando o botão de confirmação
+        if hasConfirmation {
+            let confirmAction = UIAlertAction(title: "OK", style: .default, handler: { (action) in
+                print("OK!")
+            })
+            alert.addAction(confirmAction)
+        }
+        present(alert, animated: true, completion: nil)
     }
     
     func load(show: Bool) {
@@ -50,5 +106,5 @@ class PlaceFinderViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
-    
+   
 }
