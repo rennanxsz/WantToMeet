1f95a4b7f91a9cd582c54cbcb26f5cc37bee0a1a
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-02-09 22:13:26 -0300

    Put finder points
    Adicionando os finder points.

--- WantToMeet/WantToMeet/MapViewController.swift
+++ WantToMeet/WantToMeet/MapViewController.swift
@@ -10,17 +10,41 @@ import MapKit
 
 class MapViewController: UIViewController {
     
-    @IBOutlet weak var searchBat: UISearchBar!
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var viInfo: UIView!
     @IBOutlet weak var lbName: UILabel!
     @IBOutlet weak var lbAddress: UILabel!
     
+    var places: [Place]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        searchBar.isHidden = true
+        viInfo.isHidden = true
+        
+        if places.count == 1 {
+            title = places[0].name
+        }else {
+            title = "Meus Lugares"
+        }
+        
+        for place in places {
+            addToMap(place)
+        }
+        
+        showPlaces()
+    }
     
+    func addToMap(_ place: Place){
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = place.coordinate
+        annotation.title = place.name
+        mapView.addAnnotation(annotation)
+    }
+    
+    func showPlaces() {
+        mapView.showAnnotations(mapView.annotations, animated: true)
     }
     
     @IBAction func showRoute(_ sender: UIButton) {
